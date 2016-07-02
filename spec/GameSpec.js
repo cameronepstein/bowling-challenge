4b984f8b5dc0eb8fb7305534d91a766846a15dd8
Cameron Epstein <cameron.a.epstein@gmail.com>
2016-07-02 19:09:34 +0100

    Bowling challenge restart

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -8,23 +8,27 @@ describe('Game', function() {
     game = new Game();
   });
 
-  it("starts with 10 pins standing", function() {
-    expect(game.currentPinsStanding()).toEqual(10);
+  it("can create a game", function() {
+    expect(game).not.toEqual(null);
   });
 
-  it("allows user to throw ball", function() {
-    expect(game.throwBall()).toBeUndefined();
-  });
-
-  it("changes amount of standing pins when ball is thrown", function() {
-    game.throwBall();
-    expect(game.currentPinsStanding()).toBeLessThan(11);
-  });
-
-  describe('Score', function() {
-     it('is saved after #throwBall', function() {
-       game.throwBall();
-       expect(game.saveScore()).toHaveBeenCalled();
-     });
-  });
+  // it("starts with 10 pins standing", function() {
+  //   expect(game.currentPinsStanding()).toEqual(10);
+  // });
+  //
+  // it("allows user to throw ball", function() {
+  //   expect(game.throwBall()).toBeUndefined();
+  // });
+  //
+  // it("changes amount of standing pins when ball is thrown", function() {
+  //   game.throwBall();
+  //   expect(game.currentPinsStanding()).toBeLessThan(11);
+  // });
+  //
+  // describe('Score', function() {
+  //    it('is saved after #throwBall', function() {
+  //      game.throwBall();
+  //      expect(game.saveScore()).toHaveBeenCalled();
+  //    });
+  // });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,20 +1,10 @@
 'use strict';
 
 function Game() {
-  this.DEFAULT_BOWLING_PINS = 10;
-  this.bowlingPins = this.DEFAULT_BOWLING_PINS;
+
 };
 
-var score = [];
 
 Game.prototype.currentPinsStanding = function() {
   return this.bowlingPins;
-}
-
-Game.prototype.throwBall = function() {
-  this.bowlingPins = Math.floor((Math.random() * 10) + 1);
-}
-
-Game.prototype.saveScore = function() {
-  score.push(this.bowlingPins);
 }
