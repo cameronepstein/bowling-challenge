9200be109b949039085d888557f42e331f70c855
Cameron Epstein <cameron.a.epstein@gmail.com>
2016-07-02 22:18:12 +0100

    Bowling scorer complete

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -12,18 +12,43 @@ describe('Game', function() {
     expect(game).not.toEqual(null);
   });
 
-  it("can roll a gutter game", function() {
-    for (var i = 0; i < 20; i++) {
-      game.throw(0);
-    }
+  it("can score a gutter game", function() {
+    playGame(0, 20);
     expect(game.finalScore()).toBe(0);
   });
 
   it("can complete a game scoring 1 each frame", function() {
-    for (var i = 0; i < 20; i++) {
-      game.throw(1);
-    }
+    playGame(1, 20);
     expect(game.finalScore()).toBe(20);
   });
+
+  it("can score a spare", function() {
+    game.throw(9);
+    game.throw(1);
+    game.throw(1);
+    playGame(0, 17);
+    expect(game.finalScore()).toBe(12);
+  })
+
+  it("can score a strike", function() {
+    game.throw(10);
+    game.throw(1);
+    game.throw(1);
+    playGame(0, 16);
+    expect(game.finalScore()).toBe(14);
+  })
+
+  it("can score a perfect game", function() {
+    playGame(10, 20);
+    expect(game.finalScore()).toBe(300);
+  })
+
+  // HELPERS
+
+  var playGame = function(bowlingPins, throws) {
+    for (var i = 0; i < throws; i++) {
+      game.throw(bowlingPins);
+    }
+  };
 
 });
--- src/Game.js
+++ src/Game.js
@@ -10,8 +10,47 @@ Game.prototype.throw = function(bowlingPins) {
 
 Game.prototype.finalScore = function() {
   var score = 0;
-  for (var i = 0; i < 20; i++) {
-      score += this.currentScore[i];
+  var throws = 0;
+  var game = this;
+
+  for (var frame = 0; frame < 10; frame++) {
+    isAStrike() ? addAStrike() : checkForSpare();
   }
   return score;
+
+  // HELPERS
+
+  function checkForSpare() {
+    isASpare() ? addASpare() : addNormalScore();
+  }
+
+  function isASpare() {
+    return game.currentScore[throws] + game.currentScore[throws + 1] == 10;
+  }
+
+  function addASpare() {
+    score += game.currentScore[throws] + game.currentScore[throws + 1] + game.currentScore[throws + 2];
+      throws += 2;
+  }
+
+  function isAStrike() {
+    return game.currentScore[throws] == 10;
+  }
+
+  function addAStrike() {
+    score += game.currentScore[throws] + game.currentScore[throws + 1] + game.currentScore[throws + 2];
+      throws += 1;
+  }
+
+  function addNormalScore() {
+    score += game.currentScore[throws] + game.currentScore[throws + 1];
+    throws += 2;
+  }
 };
+
+
+
+// // if foo is true, return bar(); if foo is false, bar() is skipped and false is returned
+// for the left side and quux() is returned;
+
+// ( foo && bar() ) || quux();
